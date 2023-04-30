617da6affcfe9380124f23e82ecfa1317cda2b56
Andrii Pankiv <pankivand@gmail.com>
2023-04-30 15:22:01 +0300

    initial commit + add data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+import user from '../user.json';
+import data from '../data.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
+
 export const App = () => {
   return (
     <div
