fdfe52aba1da5e0484c9414bfc28345a9e6cdc5d
Andrii Pankiv <pankivand@gmail.com>
2023-05-01 11:37:02 +0300

    fixed return in component

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,8 +1,9 @@
 import css from './FriendListItem.module.css'
 import PropTypes from 'prop-types';
 
-export const FriendListItem = ({ id, avatar, name, isOnline }) => {(
-    <li key={id} className={css.item}>
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+    return (
+    <li className={css.item}>
         {isOnline? (<span className={css.online}></span>) : (<span className={css.offline}></span>)}
         <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
         <p className={css.name}>{name}</p>
